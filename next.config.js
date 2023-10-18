259ff0f58db6c0cf5b758461f5e5f8fd102bd161
Felipe Scherer <felipescherer25@gmail.com>
2023-10-18 13:28:32 -0300

    :sparkles: feat ppwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+    pwa: {
+        dest: 'public'
+    }
+})
