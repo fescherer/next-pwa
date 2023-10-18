fe6a96e0ec74d2c0c5178c2d2ec0f6b48b257270
Felipe Scherer <felipescherer25@gmail.com>
2023-10-18 15:12:15 -0300

    :sparkles: pwa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,16 +17,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta charSet='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-        <meta name='description' content='Description' />
-        <meta name='keywords' content='Keywords' />
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
         <title>Next.js PWA Example</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link href='/favicon.ico' rel='icon' type='image/png' sizes='16x16' />
-        <link href='/favicon.ico' rel='icon' type='image/png' sizes='32x32' />
+        <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </head>
